284c7a44c4f1b61c6872981add3b29fc8db8d463
Aram Drevekenin <aram@poor.dev>
2020-03-15 11:05:29 +0100

    test(ui): adjust to new algorithm

--- src/tests/cases/ui.rs
+++ src/tests/cases/ui.rs
@@ -7,7 +7,7 @@ use std::path::PathBuf;
 use crate::start;
 
 use std::env;
-use std::fs::{File, create_dir};
+use std::fs::{File, create_dir, create_dir_all, remove_dir_all};
 use std::io::prelude::*;
 use uuid::Uuid;
 
@@ -15,12 +15,13 @@ use std::iter;
 use ::termion::event::{Event, Key};
 use crate::tests::fakes::{KeyboardEvents};
 
-fn create_root_temp_dir () -> Result<PathBuf, failure::Error> {
+fn create_root_temp_dir (name: &str) -> Result<PathBuf, failure::Error> {
     let mut dir = env::temp_dir();
-    let temp_dir_name = Uuid::new_v4();
-    dir.push(temp_dir_name.to_string());
+    dir.push(String::from("bandwhich_tests"));
+    dir.push(name.to_string());
 
-    create_dir(&dir)?;
+    remove_dir_all(&dir).ok(); // atomic remove
+    create_dir_all(&dir)?;
     Ok(dir)
 }
 
@@ -37,12 +38,15 @@ fn create_temp_file (path: PathBuf, size: usize) -> Result<(), failure::Error> {
 // TODO: also test with immediately pressing j after start without sleep to make sure we do not
 // panic
 
+// TODO: adjust tests for other platforms (currently the snapshots include the /tmp folder which
+// might not work when running on mac/windows)
+
 #[test]
 fn two_large_files_one_small_file () {
 
     let (terminal_events, terminal_draw_events, backend) = test_backend_factory(190, 50);
     let keyboard_events = sleep_and_quit_events(1);
-    let temp_dir_path = create_root_temp_dir().expect("failed to create temp dir");
+    let temp_dir_path = create_root_temp_dir("two_large_files_one_small_file").expect("failed to create temp dir");
 
     let mut file_1_path = PathBuf::from(&temp_dir_path);
     file_1_path.push("file1");
@@ -77,7 +81,7 @@ fn eleven_files () {
 
     let (terminal_events, terminal_draw_events, backend) = test_backend_factory(190, 50);
     let keyboard_events = sleep_and_quit_events(1);
-    let temp_dir_path = create_root_temp_dir().expect("failed to create temp dir");
+    let temp_dir_path = create_root_temp_dir("eleven_files").expect("failed to create temp dir");
 
     let mut file_1_path = PathBuf::from(&temp_dir_path);
     file_1_path.push("file1");
@@ -139,6 +143,52 @@ fn eleven_files () {
 }
 
 #[test]
+fn enter_folder() {
+
+    let (terminal_events, terminal_draw_events, backend) = test_backend_factory(190, 50);
+
+    let mut events: Vec<Option<Event>> = iter::repeat(None).take(1).collect();
+    events.push(Some(Event::Key(Key::Char('\n'))));
+    events.push(None);
+    events.push(Some(Event::Key(Key::Ctrl('c'))));
+    let keyboard_events = Box::new(KeyboardEvents::new(events));
+
+    let temp_dir_path = create_root_temp_dir("enter_folder").expect("failed to create temp dir");
+
+    let mut subfolder_1_path = PathBuf::from(&temp_dir_path);
+    subfolder_1_path.push("subfolder1");
+    create_dir(subfolder_1_path).expect("failed to create temporary directory");
+
+    let mut file_1_path = PathBuf::from(&temp_dir_path);
+    file_1_path.push("subfolder1");
+    file_1_path.push("file1");
+    create_temp_file(file_1_path, 8000).expect("failed to create temp file");
+
+    let mut file_2_path = PathBuf::from(&temp_dir_path);
+    file_2_path.push("file2");
+    create_temp_file(file_2_path, 4000).expect("failed to create temp file");
+
+    let mut file_3_path = PathBuf::from(&temp_dir_path);
+    file_3_path.push("file3");
+    create_temp_file(file_3_path, 4000).expect("failed to create temp file");
+
+    start(backend, keyboard_events, temp_dir_path.clone());
+    std::fs::remove_dir_all(temp_dir_path).expect("failed to remove temporary folder");
+    let terminal_draw_events_mirror = terminal_draw_events.lock().expect("could not acquire lock on terminal events");
+
+    let expected_terminal_events = vec![Clear, HideCursor, Draw, Flush, Draw, Flush, Clear, ShowCursor];
+
+    assert_eq!(
+        &terminal_events.lock().expect("could not acquire lock on terminal_events")[..],
+        &expected_terminal_events[..]
+    );
+
+    assert_eq!(terminal_draw_events_mirror.len(), 2);
+    assert_snapshot!(&terminal_draw_events_mirror[0]);
+    assert_snapshot!(&terminal_draw_events_mirror[1]);
+}
+
+#[test]
 fn move_down_and_enter_folder() {
 
     let (terminal_events, terminal_draw_events, backend) = test_backend_factory(190, 50);
@@ -151,7 +201,7 @@ fn move_down_and_enter_folder() {
     events.push(Some(Event::Key(Key::Ctrl('c'))));
     let keyboard_events = Box::new(KeyboardEvents::new(events));
 
-    let temp_dir_path = create_root_temp_dir().expect("failed to create temp dir");
+    let temp_dir_path = create_root_temp_dir("move_down_and_enter_folder").expect("failed to create temp dir");
 
     let mut file_1_path = PathBuf::from(&temp_dir_path);
     file_1_path.push("file1");
@@ -187,7 +237,7 @@ fn move_down_and_enter_folder() {
     assert_snapshot!(&terminal_draw_events_mirror[2]);
 }
 
- #[test]
+#[test]
 fn noop_when_entering_file() {
    let (terminal_events, terminal_draw_events, backend) = test_backend_factory(190, 50);
 
@@ -199,7 +249,7 @@ fn noop_when_entering_file() {
    events.push(Some(Event::Key(Key::Ctrl('c'))));
    let keyboard_events = Box::new(KeyboardEvents::new(events));
 
-   let temp_dir_path = create_root_temp_dir().expect("failed to create temp dir");
+   let temp_dir_path = create_root_temp_dir("noop_when_entering_file").expect("failed to create temp dir");
 
    let mut file_1_path = PathBuf::from(&temp_dir_path);
    file_1_path.push("file1");
@@ -244,7 +294,7 @@ fn move_up_and_enter_folder() {
     events.push(Some(Event::Key(Key::Ctrl('c'))));
     let keyboard_events = Box::new(KeyboardEvents::new(events));
 
-    let temp_dir_path = create_root_temp_dir().expect("failed to create temp dir");
+    let temp_dir_path = create_root_temp_dir("move_up_and_enter_folder").expect("failed to create temp dir");
 
     let mut subfolder_1_path = PathBuf::from(&temp_dir_path);
     subfolder_1_path.push("subfolder1");
@@ -293,7 +343,7 @@ fn move_right_and_enter_folder() {
    events.push(Some(Event::Key(Key::Ctrl('c'))));
    let keyboard_events = Box::new(KeyboardEvents::new(events));
 
-   let temp_dir_path = create_root_temp_dir().expect("failed to create temp dir");
+   let temp_dir_path = create_root_temp_dir("move_right_and_enter_folder").expect("failed to create temp dir");
 
    let mut subfolder_1_path = PathBuf::from(&temp_dir_path);
    subfolder_1_path.push("subfolder1");
@@ -338,14 +388,12 @@ fn move_left_and_enter_folder() {
    events.push(None);
    events.push(Some(Event::Key(Key::Char('h'))));
    events.push(None);
-   events.push(Some(Event::Key(Key::Char('l'))));
-   events.push(None);
    events.push(Some(Event::Key(Key::Char('\n'))));
    events.push(None);
    events.push(Some(Event::Key(Key::Ctrl('c'))));
    let keyboard_events = Box::new(KeyboardEvents::new(events));
 
-   let temp_dir_path = create_root_temp_dir().expect("failed to create temp dir");
+   let temp_dir_path = create_root_temp_dir("move_left_and_enter_folder").expect("failed to create temp dir");
 
    let mut subfolder_1_path = PathBuf::from(&temp_dir_path);
    subfolder_1_path.push("subfolder1");
@@ -354,7 +402,7 @@ fn move_left_and_enter_folder() {
    let mut file_1_path = PathBuf::from(&temp_dir_path);
    file_1_path.push("subfolder1");
    file_1_path.push("file1");
-   create_temp_file(file_1_path, 4000).expect("failed to create temp file");
+   create_temp_file(file_1_path, 8000).expect("failed to create temp file");
 
    let mut file_2_path = PathBuf::from(&temp_dir_path);
    file_2_path.push("file2");
@@ -368,19 +416,18 @@ fn move_left_and_enter_folder() {
    std::fs::remove_dir_all(temp_dir_path).expect("failed to remove temporary folder");
    let terminal_draw_events_mirror = terminal_draw_events.lock().expect("could not acquire lock on terminal events");
 
-   let expected_terminal_events = vec![Clear, HideCursor, Draw, Flush, Draw, Flush, Draw, Flush, Draw, Flush, Draw, Flush, Clear, ShowCursor];
+   let expected_terminal_events = vec![Clear, HideCursor, Draw, Flush, Draw, Flush, Draw, Flush, Draw, Flush, Clear, ShowCursor];
 
    assert_eq!(
        &terminal_events.lock().expect("could not acquire lock on terminal_events")[..],
        &expected_terminal_events[..]
    );
 
-   assert_eq!(terminal_draw_events_mirror.len(), 5);
+   assert_eq!(terminal_draw_events_mirror.len(), 4);
    assert_snapshot!(&terminal_draw_events_mirror[0]);
    assert_snapshot!(&terminal_draw_events_mirror[1]);
    assert_snapshot!(&terminal_draw_events_mirror[2]);
    assert_snapshot!(&terminal_draw_events_mirror[3]);
-   assert_snapshot!(&terminal_draw_events_mirror[4]);
 }
 
 #[test]
@@ -395,7 +442,7 @@ fn noop_when_moving_off_screen_edges() {
    events.push(Some(Event::Key(Key::Ctrl('c'))));
    let keyboard_events = Box::new(KeyboardEvents::new(events));
 
-   let temp_dir_path = create_root_temp_dir().expect("failed to create temp dir");
+   let temp_dir_path = create_root_temp_dir("noop_when_moving_off_screen_edges").expect("failed to create temp dir");
 
    let mut file_1_path = PathBuf::from(&temp_dir_path);
    file_1_path.push("file1");
@@ -440,7 +487,7 @@ fn esc_to_go_up() {
    events.push(Some(Event::Key(Key::Ctrl('c'))));
    let keyboard_events = Box::new(KeyboardEvents::new(events));
 
-   let temp_dir_path = create_root_temp_dir().expect("failed to create temp dir");
+   let temp_dir_path = create_root_temp_dir("esc_to_go_up").expect("failed to create temp dir");
 
    let mut subfolder_1_path = PathBuf::from(&temp_dir_path);
    subfolder_1_path.push("subfolder1");
@@ -493,7 +540,7 @@ fn noop_when_pressing_esc_at_base_folder() {
    events.push(Some(Event::Key(Key::Ctrl('c'))));
    let keyboard_events = Box::new(KeyboardEvents::new(events));
 
-   let temp_dir_path = create_root_temp_dir().expect("failed to create temp dir");
+   let temp_dir_path = create_root_temp_dir("noop_when_pressing_esc_at_base_folder").expect("failed to create temp dir");
 
    let mut subfolder_1_path = PathBuf::from(&temp_dir_path);
    subfolder_1_path.push("subfolder1");
