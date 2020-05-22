096bbeea78f47377cdd90066310db652e6480887
Aram Drevekenin <aram@poor.dev>
2020-05-22 16:46:15 +0200

    fix(ui): properly display size

--- src/ui/format/display_size.rs
+++ src/ui/format/display_size.rs
@@ -5,11 +5,11 @@ pub struct DisplaySize(pub f64);
 impl fmt::Display for DisplaySize{
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         if self.0 > 999_999_999.0 {
-            write!(f, "{:.1}G", self.0 / 1_000_000_000.0)
+            write!(f, "{:.1}G", self.0 / 1073741824.0) // 1024 * 1024 * 1024
         } else if self.0 > 999_999.0 {
-            write!(f, "{:.1}M", self.0 / 1_000_000.0)
+            write!(f, "{:.1}M", self.0 / 1048576.0) //  1024 * 1024
         } else if self.0 > 999.0 {
-            write!(f, "{:.1}K", self.0 / 1000.0)
+            write!(f, "{:.1}K", self.0 / 1024.0)
         } else {
             write!(f, "{}", self.0)
         }
@@ -21,11 +21,11 @@ pub struct DisplaySizeRounded(pub f64);
 impl fmt::Display for DisplaySizeRounded {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         if self.0 > 999_999_999.0 {
-            write!(f, "{:.0}G", self.0 / 1_000_000_000.0)
+            write!(f, "{:.0}G", self.0 / 1073741824.0) // 1024 * 1024 * 1024
         } else if self.0 > 999_999.0 {
-            write!(f, "{:.0}M", self.0 / 1_000_000.0)
+            write!(f, "{:.0}M", self.0 / 1048576.0) //  1024 * 1024
         } else if self.0 > 999.0 {
-            write!(f, "{:.0}K", self.0 / 1000.0)
+            write!(f, "{:.0}K", self.0 / 1024.0)
         } else {
             write!(f, "{}", self.0)
         }
