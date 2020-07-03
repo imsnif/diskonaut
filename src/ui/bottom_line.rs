e5581644c68780d3c6022f9b7436cff21b96b689
Max Heyer <mail@maxheyer.com>
2020-07-03 09:08:38 +0200

    feat(ui): change delete key #41 (#59)

--- src/ui/bottom_line.rs
+++ src/ui/bottom_line.rs
@@ -67,8 +67,8 @@ fn render_controls_legend(buf: &mut Buffer, hide_delete: bool, max_len: u16, y: 
         )
     } else {
         (
-            String::from("<arrows> - move around, <ENTER> - enter folder, <ESC> - parent folder, <Ctrl-D> - delete, <q> - quit"),
-            String::from("←↓↑→/<ENTER>/<ESC>: navigate, <Ctrl-D>: del")
+            String::from("<arrows> - move around, <ENTER> - enter folder, <ESC> - parent folder, <DELETE> - delete, <q> - quit"),
+            String::from("←↓↑→/<ENTER>/<ESC>: navigate, <DELETE>: del")
         )
     };
     let too_small_line = "(...)";
