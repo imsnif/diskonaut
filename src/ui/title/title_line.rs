bf4103fff3ed1074dca9c7badccc5e198791ae75
pm100 <paulmoore100@hotmail.com>
2020-09-22 16:05:53 -0700

    linux->unix

--- src/ui/title/title_line.rs
+++ src/ui/title/title_line.rs
@@ -9,7 +9,7 @@ use crate::ui::title::{CellSizeOpt, TitleTelescope};
 use crate::ui::FolderInfo;
 
 #[cfg(not(target_os = "windows"))]
-use crate::os::linux::is_user_admin;
+use crate::os::unix::is_user_admin;
 #[cfg(target_os = "windows")]
 use crate::os::windows::is_user_admin;
 
